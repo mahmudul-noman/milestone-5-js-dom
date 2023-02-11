94789a1308b89406d8d567050cfff7e739460ab8
Mahmudul-H-Noman <mhnoman75@gmail.com>
2023-02-11 19:38:05 +0600

    added onclick

--- module26-js-dom-event/event.js
+++ module26-js-dom-event/event.js
@@ -1,1 +1,15 @@
 console.log('Event JS File');
+
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+    document.body.style.color = '#000';
+}
+
+
+const makeBlueButton = document.getElementById('blue-btn')
+makeBlueButton.onclick = makeBlue;
+
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+    document.body.style.color = 'white';
+}
