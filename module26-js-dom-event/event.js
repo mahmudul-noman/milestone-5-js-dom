d7298178b7a830603ef06099ac7a8901d2c5625d
Mahmudul-H-Noman <mhnoman75@gmail.com>
2023-02-13 10:21:47 +0600

    added module27 folder

--- module26-js-dom-event/event.js
+++ module26-js-dom-event/event.js
@@ -1,15 +1,32 @@
-console.log('Event JS File');
+// option: 1 ::::::::::::::::::::::::::::::::
+//<button onclick="document.body.style.backgroundColor='yellow'">Color Change to Yellow</button>
 
+
+// option: 2 ::::::::::::::::::::::::::::::::
 function makeRed() {
     document.body.style.backgroundColor = 'red';
     document.body.style.color = '#000';
 }
 
 
+// option: 3 ::::::::::::::::::::::::::::::::
 const makeBlueButton = document.getElementById('blue-btn')
 makeBlueButton.onclick = makeBlue;
-
 function makeBlue(){
     document.body.style.backgroundColor = 'blue';
     document.body.style.color = 'white';
 }
+
+
+// option: 4 ::::::::::::::::::::::::::::::::
+const makeGreenBtn = document.getElementById('green-btn');
+makeGreenBtn.addEventListener('click', function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+})
+
+
+// option: 5 ::::::::::::::::::::::::::::::::
+// We will this. IMPORTANT
+document.getElementById('golden-btn').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'goldenrod';
+})
